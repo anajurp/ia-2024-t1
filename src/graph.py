cabe8cdced0baf786ea20a166e233a38f8f3ae67
Ana Julia <umanaribeiro@gmail.com>
2024-05-02 19:47:41 -0300

    estrutura de leitura do Grafo

--- src/graph.py
+++ src/graph.py
@@ -1,20 +1,21 @@
 """Implementação de uma estrutura de grafo."""
 
-import sys
 
-
-def read_graph(filename: str):
-    """Le uma estrutura de grafo de um arquivo e retorna a estrutura."""
-    graph = None
-    with open(filename, "rt") as input_file:
-        vertex_count = int(input_file.readline().strip())
-        for _ in range(vertex_count):
-            index, latitude, longitude = input_file.readline().strip().split()
-            
-        edge_count = int(input_file.readline().strip())
-        for _ in range(edge_count):
-            from_vertex, to_vertex, cost = (
-                input_file.readline().strip().split()
-            )
-    
+def read_graph(file_path):
+    """Implementação de uma estrutura de grafo."""
+    # pylint: disable=unused-variable
+    with open(file_path, encoding="utf-8") as file:
+        lines = file.read().strip().split("\n")
+    num_nodes = int(lines[0])
+    nodes = {i: tuple(map(float, line.split()[1:]))  # noqa
+             for i, line in enumerate(
+                 lines[1:num_nodes+1]
+                 )}
+    num_edges = int(lines[num_nodes+1]) # noqa
+    edges = [tuple(map(float, line.split()))
+             for line in lines[num_nodes+2:]]
+    graph = {i: [] for i in range(num_nodes)}
+    for node1, node2, cost in edges:
+        graph[node1].append((node2, cost))
+        graph[node2].append((node1, cost))
     return graph
